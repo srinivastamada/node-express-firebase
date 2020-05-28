77ade409091ded77199d3cd169525a85adc991f2
Srinivas Tamada <srinivas@9lessons.info>
2020-05-27 23:43:54 -0400

    Firebase

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+app.get('/hello', (req, res, next) => {
+  res.send('Hello');
+});
+
+app.listen(PORT, () => {
+console.log('Server is running on PORT:', PORT);
+});
+
+exports.app = functions.https.onRequest(app);
