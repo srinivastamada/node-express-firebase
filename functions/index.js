3c8d8fb49577d599b2e9fdd8427428052ab0b1be
Srinivas Tamada <srinivas@9lessons.info>
2020-06-21 15:31:06 -0400

    Update

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,33 @@
 const functions = require('firebase-functions');
 const express = require('express');
-
+const validator = require("email-validator");
 const PORT = 3000;
 const app = express();
 
 /* JSON body parse*/
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res, next) => {
+  console.info('/hello call success ');
   res.send('Welcome to Firebase Cloud Functions');
 });
 
-app.post('/validate', async (req, res, next) => {
+app.post('/emailValidate', async (req, res, next) => {
   const postData = req.body;
-  console.log(req.body)
-   res.json(postData)
-  });
+  if(postData.email){
+    console.info('/emailValidate call success ');
+    res.json({'status': validator.validate(postData.email) });
+  } else {
+    console.warn('/emailValidate wrong input ');
+    res.status(500).json({'status': 'wrong input'});
+  }
+
+});
 
 app.listen(PORT, () => {
-  console.log('Server is running on PORT:', PORT);
+  console.info('Server is running on PORT:', PORT);
 });
 
 exports.app = functions.https.onRequest(app);
