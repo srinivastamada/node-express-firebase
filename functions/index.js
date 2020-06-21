e5977afa25088894a159c23622e67b6dab3a5b58
Srinivas Tamada <srinivas@9lessons.info>
2020-06-20 23:13:19 -0400

    Post

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,27 @@
 const functions = require('firebase-functions');
 const express = require('express');
+
 const PORT = 3000;
 const app = express();
 
+/* JSON body parse*/
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
 app.get('/hello', (req, res, next) => {
-  res.send('Hello');
+  res.send('Welcome to Firebase Cloud Functions');
 });
 
+app.post('/validate', async (req, res, next) => {
+  const postData = req.body;
+  console.log(req.body)
+   res.json(postData)
+  });
+
 app.listen(PORT, () => {
-console.log('Server is running on PORT:', PORT);
+  console.log('Server is running on PORT:', PORT);
 });
 
 exports.app = functions.https.onRequest(app);
